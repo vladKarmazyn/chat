3f3b57905ca3bd4b817612249631407c0ebcdf7d
Vladyslav Karmazyn <karmazynnnn@gmail.com>
2022-09-06 20:00:51 +0300

    update

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const btn = document.querySelector('.wraper_input_button');
 const input = document.querySelector('.wraper_input_input');
-const  messageO = document.querySelector('.message0')
-const message = document.createElement('div');
-const newMessage = message.cloneNode();
-message.className = 'message';
-newMessage.className = 'message';
-function addMessage(inputM, messageM ) {
+const wrapperMessage = document.querySelector('.wraper_message');
+const close = document.querySelector('.message::after');
+
 btn.addEventListener('click', function() {
-    messageM.innerHTML = inputM.value;
-    messageO.append(messageM);
-    inputM.value = '';
+    const message = document.createElement('div');
+    message.className = 'message';
+    message.innerHTML = input.value;
+    if (input.value !== '') {
+        wrapperMessage.appendChild(message);
+        input.value = '';
+    } 
 })
+input.onkeypress = function (event) {
+    if (event.keyCode === 13 && !event.shiftKey) {
+        const message = document.createElement('div');
+        message.className = 'message';
+        message.innerHTML = input.value;
+        if (input.value !== '') {
+            wrapperMessage.appendChild(message);
+            input.value = '';
+        } 
+    }
+    if (event.keyCode === 13 && event.shiftKey) {
+        
+    }
+   
 }
-
-addMessage(input, newMessage);
-
-
-// btn.addEventListener('click', function() {
-//         message.innerHTML = input.value;
-//         messageO.append(message);
-//         input.value = '';
-//     })
