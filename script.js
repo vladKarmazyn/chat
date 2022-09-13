412d2490511938dbb832a47466edcb402c7507ae
Vladyslav Karmazyn <karmazynnnn@gmail.com>
2022-09-13 20:13:04 +0300

    delete red point

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const btn = document.querySelector('.wraper_input_button');
 const input = document.querySelector('.wraper_input_input');
 const wrapperMessage = document.querySelector('.wraper_message');
-const close = document.querySelector('.message::after');
+
 
 btn.addEventListener('click', function() {
     const message = document.createElement('div');
+
     message.className = 'message';
     message.innerHTML = input.value;
+
     if (input.value !== '') {
         wrapperMessage.appendChild(message);
         input.value = '';
