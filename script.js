6baaeb408bde4d03693e07b81d8b5197a1d168d2
Vladyslav Karmazyn <karmazynnnn@gmail.com>
2022-09-14 17:20:14 +0300

    гзвфеу

--- script.js
+++ script.js
@@ -3,24 +3,31 @@ const input = document.querySelector('.wraper_input_input');
 const wrapperMessage = document.querySelector('.wraper_message');
 
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', () => {
     const message = document.createElement('div');
 
     message.className = 'message';
     message.innerHTML = input.value;
-
+    const close = document.createElement('div');
+    close.className = 'close';
     if (input.value !== '') {
         wrapperMessage.appendChild(message);
+        message.appendChild(close);
         input.value = '';
     } 
+    console.log(message);
+    console.log(wrapperMessage);
 })
 input.onkeypress = function (event) {
     if (event.keyCode === 13 && !event.shiftKey) {
         const message = document.createElement('div');
         message.className = 'message';
         message.innerHTML = input.value;
+        const close = document.createElement('div');
+        close.className = 'close';
         if (input.value !== '') {
             wrapperMessage.appendChild(message);
+            message.appendChild(close);
             input.value = '';
         } 
     }
